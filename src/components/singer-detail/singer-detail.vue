10750440f6774db60f0494d19c0734376b72f2e2
wang <13269403044@163.com>
2018-02-01 11:32:45 +0800

    1开发业务组件music-list

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,10 +1,11 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import MusicList from 'components/music-list/music-list'
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
@@ -12,6 +13,12 @@
 
   export default {
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -47,6 +54,9 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
@@ -58,13 +68,5 @@
     transition all 0.3s
   .slide-enter, .slide-leave-to
     transform translate3d(100%, 0, 0)
-  .singer-detail
-    position fixed
-    z-index 100
-    top 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
 </style>
 
