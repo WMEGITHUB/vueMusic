61cf1575c39a6e9a01b919b0474f03e177ca4292
wang <13269403044@163.com>
2018-02-06 11:53:39 +0800

    完成热门歌单详情

---
+++ src/components/disc/disc.vue
@@ -0,0 +1,62 @@
+<template>
+  <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+</template>
+
+<script type="text/ecmascript-6">
+  import MusicList from 'components/music-list/music-list'
+  import {getSongList} from 'api/recommend'
+  import {mapGetters} from 'vuex'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+
+  export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
+    computed: {
+      title() {
+        return this.disc.dissname
+      },
+      bgImage() {
+        return this.disc.imgurl
+      },
+      ...mapGetters([
+        'disc'
+      ])
+    },
+    created () {
+      this._getSongList()
+    },
+    methods: {
+      _getSongList() {
+        if (!this.disc.dissid) {
+          this.$router.push('/recommend')
+          return
+        }
+        getSongList(this.disc.dissid).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
+    }
+  }
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -14,7 +14,7 @@
           <div class="recommend-list">
             <h1 class="list-title">热门歌单推荐</h1>
             <ul>
-              <li class="item" v-for="item in discList">
+              <li @click="selectItem(item)" class="item" v-for="item in discList">
                 <div class="icon">
                   <img width="60" height="60" v-lazy="item.imgurl">
                 </div>
@@ -41,6 +41,7 @@ import Scroll from 'base/scroll/scroll'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 import {playlistMixin} from 'common/js/mixin'
+import {mapMutations} from 'vuex'
 
 export default {
   mixins: [playlistMixin],
@@ -68,6 +69,12 @@ export default {
         this.$refs.scroll.refresh()
       }
     },
+    selectItem(item) {
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
+    },
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
@@ -81,7 +88,10 @@ export default {
           this.discList = res.data.list
         }
       })
-    }
+    },
+    ...mapMutations({
+      setDisc: 'SET_DISC'
+    })
   },
   components: {
     Slider,
