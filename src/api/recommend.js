aadcb56aea6b6e73bcc8c3d7d6360b5721a4ee28
wang <13269403044@163.com>
2018-03-07 15:01:13 +0800

    取消部署配置更改

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,8 +2,6 @@ import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 import axios from 'axios'
 
-const debug = process.env.NODE_ENV !== 'production'
-
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
@@ -17,7 +15,7 @@ export function getRecommend() {
 }
 
 export function getDiscList() {
-  const url = debug ? '/api/getDiscList' : 'http://wsaiyu.top/music/api/getDiscList'
+  const url = '/api/getDiscList'
 
   const data = Object.assign({}, commonParams, {
     platform: 'yqq',
@@ -39,7 +37,7 @@ export function getDiscList() {
 }
 
 export function getSongList(disstid) {
-  const url = debug ? '/api/getCdInfo' : 'http://wsaiyu.top/music/api/getCdInfo'
+  const url = '/api/getCdInfo'
 
   const data = Object.assign({}, commonParams, {
     disstid,
